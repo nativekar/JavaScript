f802a78db655802f6be13b445f05fdf70213b40c
nativeKar <thakuryogeshsingh@gmail.com>
2020-07-31 18:39:07 +0530

    feat(new algos): added multiple new algos

---
+++ arrayMethods.js
@@ -0,0 +1,38 @@
+/* Flat */
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+let flattenedArray = matrix.flat(1);
+console.log(flattenedArray);
+
+/* Set */
+const ary = [1, 2, 1, 2, 4, 3, 3, 5];
+const set = new Set(ary);
+console.log(set);
+
+/* reduce */
+flattenedArray = matrix.flat(1).reduce((a, v) => a + v, 0);
+console.log(flattenedArray);
+
+/*reduceRight */
+
+const str = ["t", "s", "e", "b"];
+let transformed = str.reduceRight((c, v) => c + v);
+console.log(transformed);
+
+/* from */
+
+const func = function () {
+  console.log(Array.from(arguments).join(""));
+};
+
+func("w", "h", "a", "t");
+
+/*check if the first argument passed to a function is 2 or not */
+
+const argumentsTest = (a) => {
+  const foo = a === 2 ? true : false;
+  console.log(foo);
+};
+argumentsTest(2);
--- debouncing.js
+++ debouncing.js
@@ -1,1 +1,18 @@
-const responseAccumulator = () => {};
+const responseAccumulator = (fn, d) => {
+    const timer;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            GamepadHapticActuator.apply(context, args);
+        }, d);
+    }
+};
+
+const getData = () => {
+    let counter = 0;
+    console.log(`Calling the API ${counter++} times`);
+}
+
+const debouncePolyfill = responseAccumulator(getData, 500);
