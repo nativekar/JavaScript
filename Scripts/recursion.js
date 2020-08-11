587f47dda3ddd3bf95569096bc561ac9cc8d468d
nativeKar <thakuryogeshsingh@gmail.com>
2020-08-11 21:30:41 +0530

    feat: variant added

--- Scripts/recursion.js
+++ Scripts/recursion.js
@@ -26,4 +26,47 @@ const fetchLine = (users, parent) => {
   return breadCrumb;
 };
 
-console.log(fetchLine(users, "users"));
+// console.log(fetchLine(users, "users"));
+
+const categories = {
+  id: "e_id_1",
+  electronics: {
+    mobiles: {
+      name: "Google Pixel 4",
+      id: "G101",
+    },
+    tablets: {
+      name: "Apple iPad",
+      id: "i201",
+    },
+    televisions: {
+      name: "OnePlus TV U series",
+      id: "OP301",
+    },
+  },
+  groceries: {
+    snacks: {
+      name: "Muesli",
+      id: "M102",
+    },
+    fruits: {
+      name: "Mango",
+      id: "F202",
+    },
+  },
+};
+
+const allCategories = {};
+
+const getAllCategories = (categories, parent) => {
+  for (key in categories) {
+    if (typeof categories[key] === "object")
+      getAllCategories(categories[key], parent + ">" + key);
+    else {
+      allCategories[parent + ">" + key] = categories[key];
+    }
+  }
+  return allCategories;
+};
+
+console.log(getAllCategories(categories, "categories"));
