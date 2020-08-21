c1f8c80c18c278d3e7f478264b1bbdf22dd47ac1
Thakur Yogeshwar Singh <thakuryogeshwar.s@ad.infosys.com>
2020-08-21 20:08:07 +0530

    fix: twaeked Vaibhav's logic a little to help with readbility

--- Scripts/duplicateCount.js
+++ Scripts/duplicateCount.js
@@ -1,29 +1,33 @@
-// const duplicateCount = (inputArray) => {
-//   let totalRecurringCharacters = 0;
-//   const length = inputArray.length;
-//   if (length <= 0) return totalRecurringCharacters;
-//   else {
-//     const orderedSet = new Set(inputArray);
-//     const setLength = orderedSet.size;
-//     totalRecurringCharacters = length - setLength;
-//   }
-//   return totalRecurringCharacters;
-// };
+/*This solution will work when the array has characters that don't recur more than once */
 
-const solution = (inputArray) => {
-  let numMap = {};
+const duplicateCountOne = (inputArray) => {
+  let totalRecurringCharacters = 0;
+  const length = inputArray.length;
+  if (length <= 0) return totalRecurringCharacters;
+  else {
+    const orderedSet = new Set(inputArray);
+    const setLength = orderedSet.size;
+    totalRecurringCharacters = length - setLength;
+  }
+  return totalRecurringCharacters;
+};
+
+/*This solution will work when the array has characters that recur more than once */
+
+const duplicateCountTwo = (inputArray) => {
+  let charMap = {};
   let recurringChars = 0;
   inputArray.forEach((num) => {
-    if (numMap[num] && numMap[num].count < 2) {
-      numMap[num].count += 1;
+    if (!charMap[num]) {
+      charMap[num] = { count: 1 };
+    } else if (charMap[num] && charMap[num].count < 2) {
+      charMap[num].count += 1;
       recurringChars++;
-    } else if (!numMap[num]) {
-      numMap[num] = { count: 1 };
     }
   });
+  console.log(charMap);
   return recurringChars;
 };
 
-const arr = [1, 3, 5, 3, 4, 4, 4, 5, 2, 1];
-// console.log(duplicateCount(arr));
-console.log(solution(arr));
+const arr = [1, 3, 3, 4, 4, 4];
+console.log(duplicateCountTwo(arr));
