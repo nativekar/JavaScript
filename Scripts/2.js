e52f2e9b92072525069f44846856fa2c06b6d00d
Thakur Yogeshwar Singh <thakuryogeshwar.s@ad.infosys.com>
2020-09-02 13:04:30 +0530

    chore: spacing fix

--- Scripts/2.js
+++ Scripts/2.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 class ShowsListComponent extends React.Component {
   constructor(props) {
     super(props);
--- Scripts/3.js
+++ Scripts/3.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import ShowsListComponent from "./ShowList";
 import ShowsDetailsComponent from "./ShowDetails";
 
