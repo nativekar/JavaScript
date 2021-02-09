f7f75eeec0e150b808ae64498edbde6a243de250
Thakur Yogeshwar Singh <thakuryogeshsingh@gmail.com>
2021-02-09 12:42:15 +0530

    fix: conditional check

--- Scripts/arrayMapPolyfill.js
+++ Scripts/arrayMapPolyfill.js
@@ -1,6 +1,8 @@
-Array.prototype.map = function(fn) {
+if(!Array.prototype.map){
+   Array.prototype.map = function(fn) {
     const rv = [];    
     for(let i=0, l=this.length; i<l; i++)
       rv.push(fn(this[i]));
     return rv;
+   }
   };
