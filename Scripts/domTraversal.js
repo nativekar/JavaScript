01d41fc1cdbee1cc979419674bb5448907cf5161
Thakur Yogeshwar Singh <thakuryogeshsingh@gmail.com>
2021-02-24 20:39:21 +0530

    feat: JS for find node in similar DOM tree

---
+++ Scripts/domTraversal.js
@@ -0,0 +1,34 @@
+const rootA = document.getElementById("rootA");
+const rootB = document.getElementById("rootB");
+
+const nodeA = document.getElementById("nodeA");
+const nodeB = document.getElementById("nodeB");
+
+function getPath(root, node) {
+  const path = [];
+  while (node !== root) {
+    const parent = node.parentElement;
+    const children = Array.from(parent.children);
+    const nodeIndex = children.indexOf(node);
+    path.push(nodeIndex);
+    node = parent;
+  }
+  return path;
+}
+
+function getNodeFromPath(node, path) {
+  const toWalk = [...path];
+  while (toWalk.length > 0) {
+    node = node.children[toWalk.pop()];
+  }
+  return node;
+}
+
+function getSymmetricNode(rootA, rootB, nodeA) {
+  const pathToNode = getPath(rootA, nodeA);
+  return getNodeFromPath(rootB, pathToNode);
+}
+
+const targetNode = getSymmetricNode(rootA, rootB, nodeA);
+
+console.log(nodeB === targetNode);
